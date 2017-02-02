982452dde3437e8916c16afe231f044b9507ed5a
mech <mech@themech.net>
2017-02-02 19:43:07 +0100

    Small code cleanup

--- ex2-logistic regression/1_logistic_regression.py
+++ ex2-logistic regression/1_logistic_regression.py
@@ -15,17 +15,19 @@ sns.lmplot('exam1', 'exam2', hue='admitted', data=df,
            size=6,
            fit_reg=False,
            scatter_kws={'s': 50}
-          )
+           )
 plt.show()
+
 
 # 3. Plot the sigmoid function
 # Sigmoid function
-sigmoid = lambda z: 1 / (1 + np.exp(-z))
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.plot(np.arange(-10, 10, step=0.01),
         sigmoid(np.arange(-10, 10, step=0.01)))
-ax.set_ylim((-0.1,1.1))
+ax.set_ylim((-0.1, 1.1))
 ax.set_xlabel('z', fontsize=18)
 ax.set_ylabel('g(z)', fontsize=18)
 ax.set_title('sigmoid function', fontsize=18)
@@ -45,9 +47,9 @@ W = tf.Variable(tf.zeros([numFeatures, 1]))
 b = tf.Variable(tf.zeros([1, 1]))
 
 # Sigmoid is used for the hypotesis - h(x) = x * W + b
-pred = tf.nn.sigmoid(tf.matmul(X , W) + b)
+pred = tf.nn.sigmoid(tf.matmul(X, W) + b)
 
-cost = -tf.reduce_sum(Y * tf.log(tf.clip_by_value(pred, 1e-9, 1)) + \
+cost = -tf.reduce_sum(Y * tf.log(tf.clip_by_value(pred, 1e-9, 1)) +
                       (1 - Y)*tf.log(tf.clip_by_value(1 - pred, 1e-9, 1)))
 
 correct_predict = tf.equal(tf.cast(tf.greater(pred, 0.5), tf.float32), Y)
